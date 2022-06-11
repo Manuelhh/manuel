cd5ac11893dbb56d5c32739c23e3b97dbbb17878
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2022-06-11 16:58:04 -0400

    fixed bugs

--- src/components/pageComponents/About/About.jsx
+++ src/components/pageComponents/About/About.jsx
@@ -1,5 +1,6 @@
 import s from "./About.module.css";
 import aboutImg from "../../../images/aboutPage/aboutImg.png";
+import gaImg from "../../../images/aboutPage/ga.png";
 import HomeButton from "../../generalComponents/HomeButton/HomeButton";
 
 const About = () => {
@@ -11,13 +12,19 @@ const About = () => {
       <div className={s.bottom}>
         <div className={s.aboutContainer}>
           <div className={s.aboutTextContainer}>
-            <p className={s.text}>
-              I am passionate about programming and I have extensive knowledge
-              of front-end techniques— and a flair for visual design. When I’m
-              working on a project, I love structure, order and spending time
-              with details for peak optimization. I’m especially in my element
-              when I’m coming up with original solutions while collaborating
-              with creative, talented and inspiring people.
+            <p>
+              <span>
+                General Assembly <br /> Software Engineer grad.
+                <br />
+                <sub> June to December 2021</sub>
+              </span>
+              <br />
+              <br />I am passionate about programming and I have extensive
+              knowledge of front-end techniques— and a flair for visual design.
+              When I’m working on a project, I love structure, order and
+              spending time with details for peak optimization. I’m especially
+              in my element when I’m coming up with original solutions while
+              collaborating with creative, talented and inspiring people.
             </p>
           </div>
           <div className={s.aboutImgContainer}>
--- src/components/pageComponents/Contact/Contact.jsx
+++ src/components/pageComponents/Contact/Contact.jsx
@@ -1,7 +1,53 @@
-import HomeButton from "../../generalComponents/HomeButton/HomeButton";
+// func
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+// styles
 import s from "./Contact.module.css";
+// components
+import HomeButton from "../../generalComponents/HomeButton/HomeButton";
 
 const Contact = () => {
+  const formRef = useRef(null);
+  const nameRef = useRef(null);
+  const subjectRef = useRef(null);
+  const emailRef = useRef(null);
+  const messageRef = useRef(null);
+  const [sent, setSent] = useState(false);
+
+  const handleUserData = (e) => {
+    e.preventDefault();
+    if (
+      nameRef.current.value === "" ||
+      subjectRef.current.value === "" ||
+      emailRef.current.value === "" ||
+      messageRef.current.value === ""
+    ) {
+      return;
+    }
+
+    emailjs
+      .sendForm(
+        "service_uwmyri8",
+        "template_81ytf6s",
+        formRef.current,
+        "Btd79Gvtg2pvWbjSm"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSent(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    nameRef.current.value = "";
+    subjectRef.current.value = "";
+    emailRef.current.value = "";
+    messageRef.current.value = "";
+    nameRef.current.focus();
+  };
+
   return (
     <div className={s.contactPageContainer}>
       <div className={s.topContainer}>
@@ -9,30 +55,45 @@ const Contact = () => {
       </div>
       <div className={s.bottomContainer}>
         <div className={s.contactFormContainer}>
-          <form action="" className={s.contactForm}>
+          <form action="" className={s.contactForm} ref={formRef}>
             <input
               type="text"
               placeholder="name"
               name="name"
               autocomplete="off"
+              ref={nameRef}
             />
             <input
               type="text"
               placeholder="subject"
               name="subject"
               autocomplete="off"
+              ref={subjectRef}
             />
             <input
               type="text"
               placeholder="email"
               name="email"
               autocomplete="off"
+              ref={emailRef}
             />
-            <textarea name="message" rows="5" placeholder="message"></textarea>
-            <button>send</button>
+            <textarea
+              name="message"
+              rows="5"
+              placeholder="message"
+              ref={messageRef}
+            ></textarea>
+            <button onClick={handleUserData}>send</button>
           </form>
         </div>
       </div>
+      {sent ? (
+        <div className={s.thanksMessage}>
+          <p>message sent.</p>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
